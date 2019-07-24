53ad0cdefd5a0f18a24ef5f8c112f184aff9ebeb
Harry Rong <harrykobe@gmail.com>
2019-07-24 09:44:57 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,11 +3,11 @@
 # docker-minecraft /start.sh script
 # -----------------------------------------------------------------------------
 
-server="minecraft_server.1.14.3.jar"
+server="minecraft_server.1.14.4.jar"
 
 if [ ! -f /data/$server ]
 then
-    curl "https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar" -o /data/$server
+    curl "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar" -o /data/$server
 fi
 
 if [ ! -f /data/eula.txt ]
